7182eae7110b70852742712e93330f1968aeacfb
zidol <ajh3166@naver.com>
2022-08-01 21:57:41 +0900

    Branch1에서 2번째 커밋

--- src/BranchFirst.java
+++ src/BranchFirst.java
@@ -2,5 +2,6 @@ public class BranchFirst {
     public static void main(String[] args) {
         System.out.println("Branch1 code 입니다.");
         System.out.println("Branch1에서 push 하였습니다.");
+        System.out.println("Branch1에서 2번째 push 하였습니다.");
     }
 }
