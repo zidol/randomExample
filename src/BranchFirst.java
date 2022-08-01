53d568007cba3e24904efe4ce78740ecf59e7a49
zidol <ajh3166@naver.com>
2022-08-01 21:41:03 +0900

    branch 1에서 커밋

--- src/BranchFirst.java
+++ src/BranchFirst.java
@@ -1,5 +1,6 @@
 public class BranchFirst {
     public static void main(String[] args) {
         System.out.println("Branch1 code 입니다.");
+        System.out.println("Branch1에서 push 하였습니다.");
     }
 }
