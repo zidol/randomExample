18f72798172e61c763cdca793798ead5962d46dd
zidol <ajh3166@naver.com>
2022-08-02 23:58:01 +0900

    마스터에서 커밋

--- src/BranchFirstAdd.java
+++ src/BranchFirstAdd.java
@@ -1,5 +1,6 @@
 public class BranchFirstAdd {
     public static void main(String[] args) {
         System.out.println("Branch1에서 새로 추가 한 파일입니다.");
+        System.out.println("마스터에서 커밋");
     }
 }
